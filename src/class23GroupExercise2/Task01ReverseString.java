2d9c2ecfdc00d6f4b45c6250fefa1e3f11adc9dd
Ivan D <129638650+doncevan@users.noreply.github.com>
2023-05-26 23:46:21 -0400

    Add files via upload

---
+++ src/class23GroupExercise2/Task01ReverseString.java
@@ -0,0 +1,21 @@
+package class23GroupExercise2;
+
+import java.util.Scanner;
+
+    /*
+    Reverse a String: Write a function to reverse a given string. For
+    example, given the input "Hello", the output should be "olleH".
+    */
+public class Task01ReverseString {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Your input is expected on the next line:");
+        String input = scan.nextLine();
+        String output = reverseString(input);
+        System.out.println(output);
+    }
+
+    public static String reverseString(String s) {
+        return new StringBuilder(s).reverse().toString();
+    }
+}
